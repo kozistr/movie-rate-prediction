b3749654449a9d014f8d75c90154db31c5d320c3
kozistr <kozistr@gmail.com>
2018-08-21 05:55:37 +0900

    update: w2v training + pre-processing with hannanum

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import pymysql
 
+from soynlp.normalizer import *
 from konlpy.tag import Hannanum
 from gensim.models import word2vec
 
@@ -13,13 +14,14 @@ db_infos = {
     'cursorclass': pymysql.cursors.DictCursor,
 }
 
-java_home = "C:\\Program Files\\Java\\jre-9\\bin\\server\\jvm.dll"
+jvm_path = "C:\\Program Files\\Java\\jre-9\\bin\\server\\jvm.dll"
 
 fn = "data"
-save_ext = '.csv'
+save_ext = '.'  # '.csv'
+load_ext = '.csv'
 
 
-def get_review_data() -> dict:
+def get_review_data() -> list:
     db_conn = pymysql.connect(**db_infos)
 
     data_query = "select rate, comment from movie"
@@ -57,6 +59,19 @@ def to_csv(data: list, fn: str) -> bool:
     return True
 
 
+def from_csv(fn: str) -> dict:
+    import unicodecsv as csv
+
+    data = dict()
+    try:
+        with open(fn, 'r', encoding='utf8') as f:
+            for w in csv.DictReader(f):
+                data.update(w)
+    except Exception as e:
+        raise Exception(e)
+    return data
+
+
 # Getting Review Data from DB
 dict_data = get_review_data()
 
@@ -68,3 +83,25 @@ elif save_ext == '.json':
 else:
     print("[-] Not Supporting Yet :(")
 
+# Hannanum Pos Tagger
+hannanum = Hannanum(jvmpath=jvm_path)
+
+# Analyze morphs # concat like... word/pos
+# To-Do
+# 1. Text Normalization # https://github.com/open-korean-text/open-korean-text - Done
+# 2. Text Tokenization  #
+# 3. Text Stemming
+
+emo_f = lambda x: emoticon_normalize(x, n_repeats=3)
+rep_f = lambda x: repeat_normalize(x, n_repeats=2)
+morphs_data = [list(map(lambda x: "/".join(x), rep_f(emo_f(d['comment'])))) for d in dict_data]
+
+# word2vec Training
+embeddings = word2vec.Word2Vec(sentences=morphs_data,
+                               window=5,
+                               max_vocab_size=300,
+                               min_count=5,
+                               negative=3,
+                               workers=8)
+
+embeddings.save_word2vec_format('korean_embeddings.model', binary=False)
