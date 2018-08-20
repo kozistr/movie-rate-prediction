760caf84b90bcece256c3fd56efa82b99a0bdc1c
kozistr <kozistr@gmail.com>
2018-08-20 15:43:14 +0900

    update: explict database name

--- table.sql
+++ table.sql
@@ -1,7 +1,7 @@
-create table movie (
+create table movie.movie (
   id int not null,
   movieid int not null default 0,
   rate tinyint not null default 0,
   comment varchar(640) not null default '',
   primary key (id, movieid)
-);
+)
