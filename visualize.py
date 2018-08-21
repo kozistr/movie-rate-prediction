3c68be81a1f691ffcc49032a320e3e4f1eb3c72c
kozistr <kozistr@gmail.com>
2018-08-21 15:53:41 +0900

    add: w2v visualize with t-SNE

---
+++ visualize.py
@@ -0,0 +1,60 @@
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+
+# from sklearn.manifold import TSNE
+
+from gensim.models import Word2Vec
+import matplotlib.font_manager as fm
+from MulticoreTSNE import MulticoreTSNE as TSNE
+
+
+# logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+def tsne_plot(model):
+    labels, tokens = [], []
+    
+    for w in model.wv.vocab:
+        tokens.append(model[w])
+        labels.append(w)
+    
+    config = {
+        'n_jobs': 16,
+        'perplexity': 40,
+        'n_components': 2,
+        'init': 'random',
+        'random_state': 42,
+        'verbose': 1,
+    }
+    tsne_model = TSNE(**config)
+    
+    vals = tsne_model.fit_transform(np.asarray(tokens))
+    print("[*] t-SNE training done!")
+    
+    x, y = [], []
+    for v in vals:
+        x.append(v[0])
+        y.append(v[1])
+    
+    # font
+    path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
+    fontprop = fm.FontProperties(fname=path, size=16)
+    plt.rc('font', family=fontprop.get_name())
+    
+    plt.figure(figsize=(32, 32))
+    plt.title('w2v embeddings vis')
+    
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i]) 
+        plt.annotate(labels[i], xy=(x[i], y[i]), xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right', va='bottom')
+    plt.show()
+
+
+model = Word2Vec.load('ko_w2v.model')
+print("[+] w2v model loaded!")
+tsne_plot(model)
+
