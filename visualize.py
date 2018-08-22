e17727d53b78887b4382defdc1029f2a43028fb6
kozistr <kozistr@gmail.com>
2018-08-22 16:41:37 +0900

    update: font

--- visualize.py
+++ visualize.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # from sklearn.manifold import TSNE
 
+from tqdm import tqdm
 from gensim.models import Word2Vec
 import matplotlib.font_manager as fm
 from MulticoreTSNE import MulticoreTSNE as TSNE
@@ -11,6 +12,14 @@ from MulticoreTSNE import MulticoreTSNE as TSNE
 
 # logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# font
+# path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
+path = 'C:\\Windows\\Fonts\\NanumGothic.ttf'
+font_name = fm.FontProperties(fname=path, size=16).get_name()
+plt.rc('font', family=font_name)
+
+print("[*] FONT")
 
 
 def tsne_plot(model):
@@ -21,12 +30,13 @@ def tsne_plot(model):
         labels.append(w)
     
     config = {
-        'n_jobs': 16,
+        'n_jobs': 8,
         'perplexity': 40,
         'n_components': 2,
+        'n_iter': 2500,
         'init': 'random',
         'random_state': 42,
-        'verbose': 1,
+        'verbose': 2,
     }
     tsne_model = TSNE(**config)
     
@@ -34,23 +44,21 @@ def tsne_plot(model):
     print("[*] t-SNE training done!")
     
     x, y = [], []
-    for v in vals:
+    for v in tqdm(vals):
         x.append(v[0])
         y.append(v[1])
-    
-    # font
-    # path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
-    path = 'C:\\Windows\\Fonts\\NanumGothic.ttf'
-    font_name = fm.FontProperties(fname=path, size=16).get_name()
-    plt.rc('font', family=font_name)
-    
+
+    print("[*] x, y")
+
     plt.figure(figsize=(32, 32))
     plt.title('w2v embeddings vis')
     
-    for i in range(len(x)):
+    for i in tqdm(range(len(x))):
         plt.scatter(x[i], y[i]) 
         plt.annotate(labels[i], xy=(x[i], y[i]), xytext=(5, 2),
                      textcoords='offset points', ha='right', va='bottom')
+
+    plt.imsave('tsne-w2v.png')
     plt.show()
 
 
