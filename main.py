5b672a018c147ad12ec41dd65e0d6f4a09dca574
kozistr <kozistr@gmail.com>
2018-08-23 22:35:18 +0900

    update: d2v embeddings loader

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import test as te
 import train as tr
 
 from tqdm import tqdm
-from gensim.models import Word2Vec
+from gensim.models import Word2Vec, Doc2Vec
 
 
 parser = argparse.ArgumentParser(description='train/test movie review classification model')
 parser.add_argument('--mode', type=str, help='train or test', default='train')
 parser.add_argument('--n_threads', type=int, help='the number of threads for parsing', default=8)
-parser.add_argument('--w2v_model', type=str, help='trained w2v model file', default='ko_w2v.model')
+parser.add_argument('--model', type=str, help='trained w2v/d2v model file', default='ko_w2v.model')
 parser.add_argument('--n_dims', type=int, help='embeddings'' dimensions', default=300)
+parser.add_argument('--vector', type=str, help='word2vec or doc2vec', default='d2v')
 args = parser.parse_args()
 
 
@@ -44,18 +45,52 @@ class LoadW2VEmbeddings:
         return len(self.w2v_model.wv.vocab)
 
 
+class LoadD2VEmbeddings:
+
+    def __init__(self, w2v_model, dims=300):
+        self.model = w2v_model
+
+        self.dims = dims
+
+        self.d2v_model = None
+        self.embeds = None
+
+        self.load_model()
+        self.build_embeds()
+
+    def load_model(self):
+        self.d2v_model = Doc2Vec.load(self.model)
+
+    def build_embeds(self):
+        self.embeds = np.zeros((len(self.d2v_model.wv.vocab), self.dims))
+
+        for i in tqdm(range(len(self.d2v_model.wv.vocab))):
+            vec = self.d2v_model.wv[self.d2v_model.wv.index2word[i]]
+            if vec is not None:
+                self.embeds[i] = vec
+
+    def __len__(self):
+        return len(self.d2v_model.wv.vocab)
+
+
 if __name__ == '__main__':
     # parsed args
     mode = args.mode
+    vector = args.vec
     n_dims = args.dims
-    trained_w2v_model = args.w2v_model
+    vec_model = args.vec_model
 
-    w2v = LoadW2VEmbeddings(trained_w2v_model, n_dims)
+    if vector == 'w2v':
+        vec = LoadW2VEmbeddings(vec_model, n_dims)
+    elif vector == 'd2v':
+        vec = LoadD2VEmbeddings(vec_model, n_dims)
+    else:
+        raise ValueError("[-] vector must be w2v or d2v")
 
     if mode == 'train':
         model = tr.CharCNN(dims=n_dims,
                            use_w2v=True,
-                           w2v_model=w2v.embeds)
+                           w2v_model=vec.embeds)
 
     elif mode == 'test':
         pass
