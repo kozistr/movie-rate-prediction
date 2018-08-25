5df9570c791036725827834672f54b1a6b26f18b
kozistr <kozistr@gmail.com>
2018-08-26 04:49:34 +0900

    update: DataIterator

--- dataloader.py
+++ dataloader.py
@@ -201,3 +201,39 @@ class DataLoader:
 
         for i in tqdm(range(len(self.labels))):
             self.labels[i] = arr[self.labels[i] - 1]
+
+
+class DataIterator:
+
+    def __init__(self, x, y, batch_size):
+        self.x = x
+        self.y = y
+
+        self.batch_size = batch_size
+        self.num_examples = num_examples = x.shape[0]
+        self.num_batches = num_examples // batch_size
+        self.pointer = 0
+
+        assert (self.batch_size <= self.num_examples)
+
+    def next_batch(self):
+        start = self.pointer
+        self.pointer += self.batch_size
+
+        if self.pointer > self.num_examples:
+            perm = np.arange(self.num_examples)
+            np.random.shuffle(perm)
+
+            self.x = self.x[perm]
+            self.y = self.y[perm]
+
+            start = 0
+            self.pointer = self.batch_size
+
+        end = self.pointer
+
+        return self.x[start:end], self.y[start:end]
+
+    def iterate(self):
+        for step in range(self.num_batches):
+            yield self.next_batch()
