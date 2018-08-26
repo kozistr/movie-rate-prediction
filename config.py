c78416c27eb3e262d7eb7557d677b4fb6f40383a
kozistr <kozistr@gmail.com>
2018-08-27 05:17:40 +0900

    update: args

--- config.py
+++ config.py
@@ -18,12 +18,26 @@ def get_config():
 
 # Network
 network_arg = add_arg_group('Network')
+network_arg.add_argument('--kernel_size', type=list, default=[1, 2, 3, 4],
+                         help='conv1d kernel size')
+network_arg.add_argument('--fc_unit', type=int, default=1024)
+network_arg.add_argument('--drop_out', type=int, default=.8,
+                         help='dropout rate')
 
 # DataSet
 data_arg = add_arg_group('DataSet')
+data_arg.add_argument('--embed_size', type=int, default=300,
+                      help='the size of Doc2Vec embedding vector')
+data_arg.add_argument('--batch_size', type=int, default=128)
+data_arg.add_argument('--n_threads', type=int, default=8,
+                      help='the number of workers for speeding up')
 
 # Train/Test hyper-parameters
 train_arg = add_arg_group('Training')
+train_arg.add_argument('--is_train', type=bool, default=True)
+train_arg.add_argument('--max_step', type=int, default=1e6)
+train_arg.add_argument('--optimizer', type=str, default='adam')
+train_arg.add_argument('--lr', type=float, default=8e-4)
 
 # Misc
 misc_arg = add_arg_group('Misc')
