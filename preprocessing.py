38c9a62f425132da19bd888e9f5a420a72c3094e
kozistr <kozistr@gmail.com>
2018-08-27 08:45:18 +0900

    update: refactoring codes, supports Twitter(), #3

--- preprocessing.py
+++ preprocessing.py
@@ -1,113 +1,104 @@
 import gc
-import psutil
 import pymysql
 import logging
 import argparse
 
 from tqdm import tqdm
-from gensim import corpora
 from konlpy.tag import Mecab
+from config import get_config
+
 from soynlp.normalizer import *
-from pykospacing import spacing
+
 from multiprocessing import Pool
 from collections import namedtuple
 from gensim.models import word2vec, Doc2Vec
 
 
 # Argument parser
-parser = argparse.ArgumentParser(description='Parsing NAVER Movie Review')
-parser.add_argument('--n_threads', type=int, help='the number of threads', default=5)
-parser.add_argument('--n_mem_limit', type=int, help='ram limitation', default=256)
-parser.add_argument('--max_sentences', type=int, help='the number of sentences to train (0: all)', default=2500000)
-parser.add_argument('--save_model', type=str, help='trained w2v/d2v model file', default='ko_d2v.model')
-parser.add_argument('--data_file', type=str, help='movie review data file', default=None)
-parser.add_argument('--save_dict', type=bool, help='korean words dictionary', default=False)
-parser.add_argument('--load_from', type=str, help='load DataSet from db or csv', default='db')
-parser.add_argument('--vector', type=str, help='w2v or d2v', default='d2v')
+parser = argparse.ArgumentParser(description='Pre-Processing NAVER Movie Review Comment')
+parser.add_argument('--load_from', type=str, help='load DataSet from db or csv', default='db', choices=['db', 'csv'])
+parser.add_argument('--vector', type=str, help='d2v or w2v', choices=['d2v', 'w2v'], default='d2v')
 args = parser.parse_args()
 
-db_infos = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'autoset',
-    'db': 'movie',
-    'charset': 'utf8',
-    'cursorclass': pymysql.cursors.DictCursor,
-}
+config, _ = get_config()  # global configuration
 
+# Korean Pos/Morphs Analyzer
+analyzer = None
+if config.analyzer == 'mecab':
+    from konlpy.tag import Mecab
+    analyzer = Mecab()
+elif config.analyzer == 'hannanum':
+    from konlpy.tag import Hannanum
+    analyzer = Hannanum(jvmpath=config.jvm_path)
+elif config.analyzer == 'twitter':  # Not Recommended
+    from konlpy.tag import Twitter
+    analyzer = Twitter(jvmpath=config.jvm_path)
+
+# Korean Word Spacing
+if config.use_correct_spacing:
+    try:
+        from pykospacing import spacing
+    except Exception as e:
+        raise Exception(e)
 
 vec = args.vector
 fn = args.data_file
-ko_dict = args.save_dict
 load_from = args.load_from
-model_name = args.save_model
-
-n_threads = args.n_threads
-mem_limit = args.n_mem_limit
-max_sentences = args.max_sentences
 
 
-def get_review_data() -> list:
-    db_conn = pymysql.connect(**db_infos)
+def from_db(db_cfg) -> list:
+    db_info = {
+        'host': db_cfg.host,
+        'user': db_cfg.user,
+        'password': db_cfg.password,
+        'db': db_cfg.db,
+        'charset': db_cfg.charset,
+        'cursorclass': pymysql.cursors.DictCursor,
+    }
+    db_conn = pymysql.connect(**db_info)
 
-    data_query = "select rate, comment from movie"
     with db_conn.cursor() as cur:
-        cur.execute(data_query)
-
+        cur.execute("select rate, comment from movie")
         rows = cur.fetchall()
-
     return rows
 
 
-def to_csv(data: list, fn: str) -> bool:
-    import unicodecsv as csv
+def from_csv(fn_csv: str) -> list:
+    csv_data = []
+    with open(fn_csv, 'r', encoding='utf8') as f:
+        for line in tqdm(f.readlines()[1:]):
+            dl = line.split(',')
+            try:
+                csv_data.append({'rate': dl[0], 'comment': ','.join(dl[1:])})
+            except Exception as e:
+                print(e, line)
+            del dl
+    return csv_data
 
+
+def to_csv(w_data: list, fn_csv: str) -> bool:
     try:
-        with open(fn, 'w', encoding='utf8', newline='') as f:
-            w = csv.DictWriter(f, fieldnames=['rate', 'comment'])
+        import unicodecsv as csv
 
+        with open(fn_csv, 'w', encoding='utf8', newline='') as f:
+            w = csv.DictWriter(f, fieldnames=['rate', 'comment'])
             w.writeheader()
-            for d in data:
-                w.writerow(d)
+            for d_ in w_data:
+                w.writerow(d_)
     except Exception as e:
         raise Exception(e)
     return True
 
 
-def from_csv(fn: str, sent_spacing=False) -> list:
-    global max_sentences
-
-    if max_sentences == 0:
-        max_sentences = -2
-
-    data = []
-    with open(fn, 'r', encoding='utf8') as f:
-        for line in tqdm(f.readlines()[1:max_sentences + 1]):
-            d = line.split(',')
-            try:
-                # remove dirty stuffs
-                if sent_spacing:
-                    data.append({'rate': d[0],
-                                 'comment': spacing(','.join(d[1:]).replace('\x00', '').replace('\n', '').
-                                                    replace('<span class=""ico_penel""></span>', '').strip('"').strip())
-                                 })
-                else:
-                    data.append({'rate': d[0],
-                                 'comment': ','.join(d[1:]).replace('\x00', '').replace('\n', '').
-                                replace('<span class=""ico_penel""></span>', '').strip('"').strip()})
-            except Exception as e:
-                print(e, line)
-            del d
-    return data
+def word_cleaning(w_data: list) -> list:
+    w_len = len(w_data)
+    for w_idx in tqdm(range(w_len)):  # faster way than w_data iter
+        w_data[w_idx]['comment'] = w_data[w_idx]['comment'].replace('<span class=""ico_penel""></span>', '').\
+            replace('\x00', '').replace('\n', '').strip('"').strip()
+    return w_data
 
 
-def word_processing(data: list) -> (list, list):
-    # Hannanum
-    # hannanum = Hannanum()
-
-    # Mecab Pos Tagger
-    mecab = Mecab()
-    
+def word_processing(w_data: list) -> (list, list):
     def emo(x: str) -> str:
         return emoticon_normalize(x, n_repeats=3)
 
@@ -118,25 +109,18 @@ def word_processing(data: list) -> (list, list):
         return rep(emo(x))
 
     idx = 0
-    n_data = len(data)
+    n_data = len(w_data)
     p_data, l_data = [], []
-    for d in data:
-        pos = list(map(lambda x: '/'.join(x), mecab.pos(normalize(d['comment']))))
+    for wd in w_data:
+        pos = list(map(lambda x: '/'.join(x), analyzer.pos(normalize(wd['comment']))))
 
         # append sentence & rate
         p_data.append(pos)
         l_data.append(d['rate'])
 
-        if idx > 0 and idx % (n_data // (100 * n_threads)) == 0:
+        if idx > 0 and idx % (n_data // (100 * config.n_threads)) == 0:
             print("[*] %d/%d" % (idx, n_data), pos)
             gc.collect()
-            
-            remain_ram = psutil.virtual_memory().available / (2**20)
-            if remain_ram < mem_limit:
-                import sys
-                print("[-] not enough memory < 256MB, ", remain_ram)
-                sys.exit(-1)
-
         del pos
         idx += 1
     return p_data, l_data
@@ -202,27 +186,29 @@ def d2v_training(sentences: list, rates: list, epochs=10) -> bool:
     return True
 
 
-# Getting Review Data from DB/CSV
-if load_from == 'db':
-    data = get_review_data()
-    if fn:  # save to .csv
-        to_csv(data, fn)
-elif load_from == 'csv':
-    data = from_csv(fn)
-else:
-    raise ValueError("[-] load_from argument must be 'db' or 'csv")
+def main():
+    # Stage 1 : Parsing Data from DB or .csv(file)
+    if load_from == 'db':
+        raw_data = from_db(config)
+    else:
+        raw_data = from_csv(config)
 
-gc.collect()
+    # Stage 2 : Cleaning Data
+    cleaned_data = word_cleaning(raw_data)
+    del raw_data  # release unused memory
 
-# Analyze morphs # concat like... word/pos
-# To-Do
-# * Text Normalizing
-# * Text Stemming
+    # Stage 3 : word processing
+    x_data, y_data = word_processing(cleaned_data)
+
+
+if __name__ == "__main__":
+    main()
+
 
 x_data, y_data = [], []
 ts = len(data) // n_threads  # 5366474
 with Pool(n_threads) as p:
-    pp_data = [p.apply_async(word_processing, (data[ts * i:ts * (i + 1)],)) for i in range(n_threads)]
+    pp_data = [p.apply_async(word_processing, (data[ts * i:ts * (i + 1)],)) for i in range(config.n_threads)]
     
     for d in pp_data:
         x_data += d.get()[0]
@@ -233,9 +219,6 @@ gc.collect()
 
 # logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-if ko_dict:
-    corpora.Dictionary(x_data).save('ko.dict')
 
 if vec == 'd2v':
     d2v_training(x_data, y_data)  # d2v Training
