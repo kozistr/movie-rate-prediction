6256fc10aa99a0e9747378aedecf517e879af5c3
kozistr <kozistr@gmail.com>
2018-08-27 05:07:00 +0900

    update: funcs stuff

--- config.py
+++ config.py
@@ -1,2 +1,25 @@
 import argparse
 
+
+args_list = []
+parser = argparse.ArgumentParser()
+
+
+def add_arg_group(name: str):
+    arg = parser.add_argument_group(name)
+    args_list.append(arg)
+    return arg
+
+
+def get_config():
+    cfg, un_parsed = parser.parse_known_args()
+    return cfg, un_parsed
+
+
+# Network
+
+# DataSet
+
+# Train/Test hyper-parameters
+
+# Misc
