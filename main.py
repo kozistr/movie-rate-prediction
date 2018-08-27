70580495d723a6fa0db93a092b57c57759569e16
kozistr <kozistr@gmail.com>
2018-08-27 12:32:45 +0900

    update: refactoring, v0.6

--- main.py
+++ main.py
@@ -4,72 +4,55 @@ import numpy as np
 import tensorflow as tf
 
 from model import charcnn
+from config import get_config
 from dataloader import Doc2VecEmbeddings, DataLoader, DataIterator
 
 
 parser = argparse.ArgumentParser(description='train/test movie review classification model')
-parser.add_argument('--mode', type=str, help='train or test', default='train')
-parser.add_argument('--model_name', type=str, help='classification model', default='charcnn')
 parser.add_argument('--checkpoint', type=str, help='pre-trained model', default=None)
-parser.add_argument('--dataset', type=str, help='DataSet path', default='tagged_data.csv')
-parser.add_argument('--n_threads', type=int, help='the number of threads', default=8)
-parser.add_argument('--model', type=str, help='trained w2v/d2v model file', default='ko_d2v.model')
-parser.add_argument('--n_dims', type=int, help='embeddings'' dimensions', default=300)
-parser.add_argument('--n_classes', type=int, help='the number of classes', default=10)
-parser.add_argument('--seed', type=int, help='random seed', default=1337)
-parser.add_argument('--save_to_file', type=bool, help='save DataSet into .csv file', default=True)
-parser.add_argument('--save_file', type=str, help='DataSet file name', default='tagged_data.csv')
-parser.add_argument('--verbose', type=bool, help='print progress', default=True)
 args = parser.parse_args()
 
 # parsed args
-mode = args.mode
-seed = args.seed
-n_dims = args.n_dims
-dataset = args.dataset
-vec_model = args.model
-verbose = args.verbose
-n_classes = args.n_classes
-n_threads = args.n_threads
 checkpoint = args.checkpoint
-model_name = args.model_name
 
-save_to_file = args.save_to_file
-save_file = args.save_file
+# Configuration
+config, _ = get_config()
 
-np.random.seed(seed)
-tf.set_random_seed(seed)
+np.random.seed(config.seed)
+tf.set_random_seed(config.seed)
 
 
 if __name__ == '__main__':
     # DataSet Loader
-    ds = DataLoader(dataset,
-                    n_classes=n_classes,
-                    is_tagged_file=True,
-                    save_to_file=save_to_file,
-                    use_in_time_save=False,
-                    # save_file=save_file,
-                    n_threads=n_threads)
+    ds = DataLoader(file=config.processed_dataset,
+                    n_classes=config.n_classes,
+                    analyzer=config.analyzer,
+                    is_analyzed=True,
+                    use_save=False,
+                    config=config)
 
-    if verbose:
+    if config.verbose:
         print("[+] DataSet loaded! Total %d samples" % len(ds.labels))
 
+    # DataSet Iterator
+    di = DataIterator(x=ds.sentences, y=ds.labels, batch_size=config.batch_size)
+
     # Doc2Vec Loader
-    vec = Doc2VecEmbeddings(vec_model, n_dims)
-    if verbose:
+    vec = Doc2VecEmbeddings(config.d2v_model, config.embed_size)
+    if config.verbose:
         print("[+] Doc2Vec loaded! Total %d pre-trained sentences, %d dims" % (len(vec), n_dims))
 
-    if mode == 'train':
+    if config.is_train:
         # GPU configure
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
 
         with tf.Session(config=config) as s:
-            if model_name == 'charcnn':
+            if config.model == 'charcnn':
                 # Model Loaded
                 model = charcnn.CharCNN(s=s,
-                                        n_classes=n_classes,
-                                        dims=n_dims)
+                                        n_classes=config.n_classes,
+                                        dims=config.embed_size)
             else:
                 raise NotImplementedError("[-] Not Implemented Yet")
 
@@ -89,19 +72,10 @@ if __name__ == '__main__':
             else:
                 print('[-] No checkpoint file found')
 
-            # DataSet Iterator
-            di = DataIterator(x=ds.sentences, y=ds.labels,
-                              batch_size=model.batch_size)
-
-            # To-Do
-            # pre-trained model loader
-
             start_time = time.time()
 
-            epochs = 10
             global_step = 0
-            logging_step = 1000
-            for epoch in range(epochs):
+            for epoch in range(config.epochs):
                 for x_train, y_train in di.iterate():
                     # training
                     loss = s.run([model.opt, model.loss],
@@ -111,8 +85,8 @@ if __name__ == '__main__':
                                      model.do_rate: .8,
                                  })
 
-                    if global_step % logging_step == 0:
-                        print("[*] epoch %d global step %d" % (epoch, global_step), " loss : {:.8f}",format(loss))
+                    if global_step % config.logging_step == 0:
+                        print("[*] epoch %d global step %d" % (epoch, global_step), " loss : {:.8f}".format(loss))
 
                         summary = s.run([model.merged],
                                         feed_dict={
@@ -125,7 +99,7 @@ if __name__ == '__main__':
                         model.writer.add_summary(summary, global_step)
 
                         # Model save
-                        model.saver.save(s, './model/%s.ckpt' % model_name, global_step=global_step)
+                        model.saver.save(s, './model/%s.ckpt' % config.model, global_step=global_step)
 
                     global_step += 1
 
@@ -133,7 +107,5 @@ if __name__ == '__main__':
 
             print("[+] Training Done! Elapsed {:.8f}s".format(end_time - start_time))
 
-    elif mode == 'test':
+    else:  # Test
         pass
-    else:
-        print('[-] mode should be train or test')
